9b74dfbcd01afc82ffdf94d2cc071ac1dbcbcfd1
Ehsanullah Jan <ehsanullahjan@gmail.com>
2016-01-15 00:04:04 -0500

    Added a stub impl. of Stack for Kainat to implement a fixed-size stack

---
+++ src/org/practice/algorithms/stack/FixedArrayStack.java
@@ -0,0 +1,35 @@
+package org.practice.algorithms.stack;
+
+import org.practice.algorithms.adt.Stack;
+
+public class FixedArrayStack<T> implements Stack<T> {
+    @Override
+    public void push(T item) {
+
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
