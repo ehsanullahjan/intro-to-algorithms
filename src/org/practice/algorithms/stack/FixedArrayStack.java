0d510758a942b1f0f3ff8190d7f33d99d7a6d978
Kainat <kainatjan96@gmail.com>
2016-01-24 21:33:29 +0500

    Abstract stack is implemented.

--- src/org/practice/algorithms/stack/FixedArrayStack.java
+++ src/org/practice/algorithms/stack/FixedArrayStack.java
@@ -3,33 +3,100 @@ package org.practice.algorithms.stack;
 import org.practice.algorithms.adt.Stack;
 
 public class FixedArrayStack<T> implements Stack<T> {
+    @SuppressWarnings("unchecked")
+    private Object[] items;
+    int count;
+
+    public FixedArrayStack(int size) {
+        items = new Object[size];
+        count = 0;
+    }
+
     @Override
     public void push(T item) {
-
+        if (count > 7) {
+            throw new IllegalStateException("Stack overflow");
+        }
+        items[count] = item;
+        count++;
     }
 
     @Override
     public T pop() {
-        return null;
+        if(items[0]==null){
+            throw new IllegalStateException("Stack underflow");
+        }
+        Object ElementSave = items[items.length - 1];
+        items[items.length - 1] = null;
+        count--;
+        return (T) ElementSave;
     }
 
     @Override
     public T peek() {
-        return null;
+        if(items[0]==null){
+           throw new IllegalStateException("Stack underflow");
+            }
+        return (T) items[items.length - 1];
     }
 
     @Override
     public int size() {
-        return 0;
+        return items.length;
     }
 
     @Override
     public boolean isFull() {
-        return false;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean isEmpty() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Stack<String> stack = new FixedArrayStack<>(8);
+        assert stack.isEmpty() : "New stack must be empty";
+
+        stack.push("Johny");
+        stack.push("Manny");
+        assert stack.size() == 2;
+        assert !stack.isEmpty();
+
+        assert stack.pop().equals("Manny");
+        assert stack.size() == 1;
+        assert !stack.isEmpty();
+
+        assert stack.peek().equals("Johny");
+        assert stack.size() == 1;
+
+        assert stack.pop().equals("Johny");
+        assert stack.size() == 0;
+        assert stack.isEmpty();
+
+        /*
+        if (stack is full) {
+            throw new IllegalStateException("Stack overflow");
+        }
+
+        ...
+
+
+        if (stack is empty) {
+            throw new IllegalStateException("Stack underflow");
+        }
+
+       */
     }
 }
